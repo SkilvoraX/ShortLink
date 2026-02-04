40f78c7e3497077df726a9c66d068247c62eeee5
Lyberia-01 <oselumesesamuel@gmail.com>
2026-02-04 10:29:32 +0100

    fix: Corrected the Google Auth Flow to allow users login with google account easily

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -80,3 +80,32 @@ export const signIn = async (req: Request, res: Response): Promise<void> => {
     res.status(500).json({ error: 'Login failed' });
   }
 };
+
+export const google = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(401).json({ error: 'Invalid email or password' });
+      return;
+    }
+    console.log(user._id.toString());
+
+    const { accessToken, refreshToken } = generateTokens(user._id.toString());
+
+   res.json({
+  user: {
+    id: user._id,
+    name: user.fullName,
+    email: user.email
+  },
+  accessToken,
+  refreshToken
+})
+
+  } catch (error) {
+    console.error('Login error:', error);
+    res.status(500).json({ error: 'Login failed' });
+  }
+};
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
-import { signUp, signIn } from '../controllers/authController';
+import { signUp, signIn, google } from '../controllers/authController';
 
 const authRoutes = Router();
 
 
 authRoutes.post('/signup', signUp);
 
-
 authRoutes.post('/signin', signIn);
+
+authRoutes.post('/google', google);
 
 export default authRoutes;
